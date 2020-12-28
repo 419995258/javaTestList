bed2cf3e9b25228b959fc88a7bbdc228c3440fbe
pengbin <419995258@qq.com>
2020-12-28 17:12:37 +0800

    修改实现逻辑

--- src/main/java/leecode/norm/优势洗牌870xdf.java
+++ src/main/java/leecode/norm/优势洗牌870xdf.java
@@ -29,7 +29,7 @@ import java.util.List;
  * 0 <= A[i] <= 10^9
  * 0 <= B[i] <= 10^9
  *
- * 来源：力扣（LeetCode）1
+ * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/advantage-shuffle
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
